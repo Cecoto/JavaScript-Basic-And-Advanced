2829cdafbab6d857e0fe640dd81ba7d548a1ae4c
Tsvetomir <cecohristov97@gmail.com>
2022-10-03 20:03:19 +0300

    P02 - Delete from table

---
+++ DOM-ManipulationAndEvents.Lab/02. Delete-From-Table/app.js
@@ -0,0 +1,27 @@
+function deleteByEmail() {
+    let tableRows = document.querySelectorAll('tbody tr');
+
+
+    let input = document.querySelector('label input').value;
+
+    let isFound = false;
+    for (const row of tableRows) {
+
+        if (row.children[1].textContent == input) {
+            const parent = row.parentElement;
+            parent.removeChild(row);
+            isFound = true;
+        }
+       
+    }
+    if (isFound) {
+        document.getElementById('result').textContent = 'Deleted.';
+    }
+    else {
+        document.getElementById('result').textContent = "Not found.";
+
+    }
+
+
+
+}
