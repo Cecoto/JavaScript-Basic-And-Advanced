7f3a7090be3201dc369520c90f334d13c4a25f4f
Tsvetomir <cecohristov97@gmail.com>
2022-10-19 22:59:02 +0300

    P06 - Dynamic validation

---
+++ Unit Testing and Error Handling - Exercise/06.Dynamic-Validation/app.js
@@ -0,0 +1,14 @@
+function validate() {
+    let emailInput = document.getElementById('email');
+    let pattern = new RegExp('^[a-z]+@[a-z]+\\.[a-z]+');
+
+    emailInput.addEventListener('change',()=>{
+        if (pattern.test(emailInput.value)) {
+            emailInput.classList.remove('error');
+        }
+        else{
+            emailInput.classList.add('error');
+
+        }
+    });
+}
