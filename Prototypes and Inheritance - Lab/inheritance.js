7ebb70ce8dfde6324c04099fd5e5bb57f471b604
Tsvetomir <cecohristov97@gmail.com>
2022-10-27 23:54:53 +0300

    inheritance - exercise

---
+++ Prototypes and Inheritance - Lab/inheritance.js
@@ -0,0 +1,24 @@
+class Button {
+    constructor(label) {
+        this.lavel = label;
+    }
+
+    click() {
+        console.log(`${this.label} is clicked`);
+    }
+}
+class FanctButton extends Button {
+    constructor(label, color) {
+        super(label);
+        this.color = color;
+    }
+
+    glow(){
+        console.log(`The button glows in ` + this.color);
+    }
+}
+
+const btn = new FanctButton('Print', 'green');
+
+btn.click();
+btn.glow();
