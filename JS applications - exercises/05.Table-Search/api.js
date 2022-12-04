7da2abc82878b10960916db6601a6608b0c0c2f3
Tsvetomir <cecohristov97@gmail.com>
2022-12-05 00:16:06 +0200

    Furniture

--- JS applications - exercises/05.Table-Search/api.js
+++
@@ -1,8 +0,0 @@
-let url = "http://localhost:3030/jsonstore/advanced/table";
-
-export async function getData() {
-
-    let response = await fetch(url);
-    let data = await response.json();
-    return data;
-}
